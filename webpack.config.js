9f3020f1d9e3552a150069cc8d49ef37ec83c296
Zekumoru Dragonhart <dev@zekumoru.com>
2022-09-15 18:05:08 +0200

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    watchFiles: [
+      'src/*.html'
+    ],
+    port: 3000,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/template.html'),
+    }),
+  ],
+};

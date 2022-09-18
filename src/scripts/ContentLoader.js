e683c86fd0ccc1250a01028d902b6deef84c33f4
Zekumoru Dragonhart <dev@zekumoru.com>
2022-09-19 00:52:18 +0200

    Write load, loadBefore, and loadAfter

--- src/scripts/ContentLoader.js
+++ src/scripts/ContentLoader.js
@@ -1,9 +1,40 @@
 export default {
-  load
+  load,
+  loadBefore,
+  loadAfter,
 };
 
 const content = document.querySelector('.content');
+const elements = {
+  get content() { return content; },
+  set content(value) { throw TypeError('Cannot set property \'content\', it is reserved and unmodifiable'); }
+};
 
-function load(element) {
-  content.appendChild(element);
+function load(element, name = '') {
+  if (name) elements[name] = element;
+  content.append(element);
+}
+
+function loadBefore(before, element, name = '') {
+  if (name) elements[name] = element;
+
+  if (before === 'content') {
+    content.prepend(element);
+    return;
+  }
+
+  if (!elements[before]) throw TypeError(`Cannot load before '${before}' since it does not exist`);
+  elements[before].insertAdjacentElement('beforebegin', element);
+}
+
+function loadAfter(after, element, name = '') {
+  if (name) elements[name] = element;
+  
+  if (after === 'content') {
+    content.append(element);
+    return;
+  }
+
+  if (!elements[after]) throw TypeError(`Cannot load after '${after}' since it does not exist`);
+  elements[after].insertAdjacentElement('afterend', element);
 }

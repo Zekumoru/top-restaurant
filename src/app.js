50adcde1f28da25138403248ddd8b58e5903b526
Zekumoru Dragonhart <dev@zekumoru.com>
2022-09-19 02:27:17 +0200

    Move HTML content to be generated by JavaScript

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 import ContentLoader from './scripts/ContentLoader';
+import loadPrimaryHeader from './scripts/loadPrimaryHeader';
+import loadPrimaryNav from './scripts/loadPrimaryNav';
+import loadFooter from './scripts/loadFooter';
 import './styles.css';
+
+ContentLoader.loadFirst(loadPrimaryHeader(), 'primary-header');
+ContentLoader.loadAfter(loadPrimaryNav(), 'primary-header', 'primary-nav');
+ContentLoader.load(loadFooter(), 'footer')

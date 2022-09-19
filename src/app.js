c2c09232ff10be38b5409a2fa9dda14ad55866ad
Zekumoru Dragonhart <dev@zekumoru.com>
2022-09-19 02:58:46 +0200

    Load home content

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import ContentLoader from './scripts/ContentLoader';
 import loadPrimaryHeader from './scripts/loadPrimaryHeader';
 import loadPrimaryNav from './scripts/loadPrimaryNav';
 import loadFooter from './scripts/loadFooter';
+import loadHomeContents from './scripts/loadHomeContents';
 import './styles.css';
 
 ContentLoader.loadFirst(loadPrimaryHeader(), 'primary-header');
 ContentLoader.loadAfter(loadPrimaryNav(), 'primary-header', 'primary-nav');
 ContentLoader.load(loadFooter(), 'footer')
+
+ContentLoader.loadMultipleAfter(loadHomeContents().reverse(), 'primary-nav');
